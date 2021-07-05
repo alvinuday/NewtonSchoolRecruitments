88af2956615d1f36c50b0a3df96197bd4da08871
Alvin Adarsh <udayalvin@gmail.com>
2021-07-05 11:48:37 +0530

    chnges

--- diagonalSum.c
+++ diagonalSum.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 
 int main(){
-    int arr[3][3];
-    int p;
-    int s;
-    for (int i=0;i<3;i++){
-        for (int j=0;j<3;j++){
+    int n;
+    printf("Enter N\n");
+    scanf("%d",&n);
+    int arr[n][n];
+    int p=0,s=0;
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
             int temp;
             scanf("%d",&temp);
             arr[i][j] = temp;
         }
     }
-    p = arr[0][0]+arr[1][1]+arr[2][2];
-    s = arr[0][2]+arr[1][1]+arr[2][0];
-    printf("The primary diagonal is %d:\n",p);
-    printf("The secondary diagonal is %d:\n",s);
+    /* p = arr[0][0]+arr[1][1]+arr[2][2];
+    s = arr[0][2]+arr[1][1]+arr[2][0]; */
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j){
+                p+=arr[i][j];
+            }
+            if((i+j)==(n-1)){
+                s+=arr[i][j];
+            }
+        }
+    }
+    printf("The primary diagonal is: %d\n",p);
+    printf("The secondary diagonal is: %d\n",s);
     return 0;
 }
